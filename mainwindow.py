538d5411e10298f431bb9989e94886c1098ad3f3
cdcpku2024 <1524531922@qq.com>
2026-05-20 00:38:26 +0800

    Remove unused import of sys module

--- mainwindow.py
+++ mainwindow.py
@@ -4,7 +4,6 @@ from ui.ui_form import Ui_MainWindow
 from app.editor_manager import EditorManager
 from app.cpp_runner import CppRunner
 from app.panel_manager import PanelManager
-import sys
 
 
 class MainWindow(QMainWindow):
